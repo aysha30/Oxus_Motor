b75e97eb5f9e8cf749eb10f3efaca808881539e5
Hritik Khetan <39329476+hritikkhetan@users.noreply.github.com>
2021-06-11 22:00:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const URI = 'mongodb+srv://hritik:123@freecluster.51vdj.mongodb.net/OxusMotors?retryWrites=true&w=majority'
+const app = express()
+
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('db connected..!');
+};
+
+connectDB()
+
+app.use(express.json())
+
+const carRouter = require('./routes/cars')
+app.use('/cars', carRouter)
+
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, () => {
+  console.log('Server started')
+})
