1a1a57050d6dc7daacfff24969d86b0d2d54f04f
Hritik Khetan <39329476+hritikkhetan@users.noreply.github.com>
2021-06-15 13:11:41 +0530

    Update car.js

--- models/car.js
+++ models/car.js
@@ -16,71 +16,65 @@ const carSchema = new mongoose.Schema({
         type: String,
         required: false
     },
-    basic_specs: {
-        type: [{
-            mileage: {
-                type: String,
-                required: true
-            },
-            engine: {
-                type: String,
-                required: true
-            },
-            drive_train: {
-                type: String,
-                required: true
-            },
-            style_name: {
-                type: String,
-                required: true
-            },
-            body_style: {
-                type: String,
-                required: true
-            },
-            brake_type: {
-                type: String,
-                required: true
-            },
-            air_bags: {
-                type: String,
-                required: true
-            },
-            night_vision: {
-                type: String,
-                required: true
-            },
-            passanger_capacity: {
-                type: Number,
-                required: true
-            },
-            abs_brake: {
-                type: String,
-                required: true
-            },
-            backup_cam: {
-                type: String,
-                required: true
-            },
-            stereo_sound: {
-                type: String,
-                required: true
-            },
-            traction_control: {
-                type: String,
-                required: true
-            },
-            parking_assistant: {
-                type: String,
-                required: true
-            },
-            mp3_player: {
-                type: String,
-                required: true
-            }
-        }],
+    mileage: {
+        type: String,
         required: true
     },
-    features: Object
+    engine: {
+        type: String,
+        required: true
+    },
+    drive_train: {
+        type: String,
+        required: true
+    },
+    style_name: {
+        type: String,
+        required: true
+    },
+    body_style: {
+        type: String,
+        required: true
+    },
+    brake_type: {
+        type: String,
+        required: true
+    },
+    air_bags: {
+        type: String,
+        required: true
+    },
+    night_vision: {
+        type: String,
+        required: true
+    },
+    passanger_capacity: {
+        type: Number,
+        required: true
+    },
+    abs_brake: {
+        type: String,
+        required: true
+    },
+    backup_cam: {
+        type: String,
+        required: true
+    },
+    stereo_sound: {
+        type: String,
+        required: true
+    },
+    traction_control: {
+        type: String,
+        required: true
+    },
+    parking_assistant: {
+        type: String,
+        required: true
+    },
+    mp3_player: {
+        type: String,
+        required: true
+    }
 })
 module.exports = mongoose.model('Car', carSchema)
