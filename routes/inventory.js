a9c61ffc56f02eba78ec93eba152dde703495335
Hritik Khetan <39329476+hritikkhetan@users.noreply.github.com>
2021-06-16 15:15:56 +0530

    Add files via upload

---
+++ routes/inventory.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const Car = require('../models/car')
+
+router.get('/', async(req, res) => {
+    try{
+        const cars = await Car.find()
+        res.json({
+            "data": cars,
+            "message": "List of all cars",
+            "status": true,
+            "code": 200
+        })
+    }catch(err){
+        res.send('Error: ' + err)
+    }
+})
+
+router.get('/name/:name', async(req, res) => {
+    try{
+        const cars = await Car.find({'name': req.body.name})
+        res.json({
+            "data": cars,
+            "message": "List of all " + req.body.name + " cars.",
+            "status": true,
+            "code": 200
+        })
+    }catch(err){
+        res.send('Error: ' + err)
+    }
+})
+
+router.get('/body_style/:body_style', async(req, res) => {
+    try{
+        const cars = await Car.find({'body_style': req.body.body_style})
+        res.json({
+            "data": cars,
+            "message": "List of all cars with " + req.body.body_style + " body style.",
+            "status": true,
+            "code": 200
+        })
+    }catch(err){
+        res.send('Error: ' + err)
+    }
+})
+
+module.exports = router
